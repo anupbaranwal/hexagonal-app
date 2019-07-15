51204fcf8da4b735e49e3677c8729d9120353b49
akumar2080317 <anup.kumar2@socgen.com>
2019-07-15 17:17:08 +0530

    feat(jpa-adapter): added DB config for Jpa adapter

--- hexagonal-jpa-adapter/src/main/java/com/example/hexagonal/infra/entity/Music.java
+++ hexagonal-jpa-adapter/src/main/java/com/example/hexagonal/infra/entity/Music.java
@@ -1,17 +1,19 @@
 package com.example.hexagonal.infra.entity;
 
 import lombok.Data;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
 
 @Table(name = "T_MUSIC")
 @Entity
 @Data
 public class Music {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "ID")
     private Long id;
 
---
+++ hexagonal-jpa-adapter/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO T_MUSIC(ID, LYRICS, AUTHOR_NAME, RATING) VALUES
+(1, 'Music 1', 'anup', 4),
+(2, 'Music 2', 'anup', 4),
+(3, 'Music 3', 'anup', 4),
+(4, 'Music 4', 'anup', 4);
