4e2de8c0bb4d78e29e8cf7ee398b4f418d3d379d
Anup Kumar <anup.raj1989@gmail.com>
2019-08-04 20:29:28 +0530

    fix(rest-adapter): Corrected the api url to follow standards

--- hexagonal-rest-adapter/src/main/java/com/example/hexagonal/rest/MusicResource.java
+++ hexagonal-rest-adapter/src/main/java/com/example/hexagonal/rest/MusicResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/musics")
+@RequestMapping("/api/v1/musics")
 public class MusicResource {
 
     @Autowired
